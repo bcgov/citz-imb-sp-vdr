3dceced69dc5cf5fdc70b5ca53e9132eeb071446
SDToews <33942967+SDToews@users.noreply.github.com>
2020-01-27 10:49:04 -0800

    add app-root div to home page

--- src/index.js
+++ src/index.js
@@ -6,13 +6,22 @@ import * as serviceWorker from './serviceWorker';
 import $ from 'jquery'
 
 $(document).ready(function () {
-    ReactDOM.render(<App value='test'/>, document.getElementById('app-root'));
+    let pageName = window.location.pathname.split("/")
+
+    if (pageName[pageName.length - 1] === 'Home.aspx') {
+        $("#DeltaPlaceHolderMain").append("<div id='app-root'></div>")
+    }
+
     var head = document.getElementsByTagName('head')[0]
     var vdrStyle = document.createElement('link')
     vdrStyle.type = 'text/css'
     vdrStyle.rel = 'stylesheet'
     vdrStyle.href = '../SiteAssets/css/vdr.css'
     head.appendChild(vdrStyle)
+
+
+
+    ReactDOM.render(<App value='test' />, document.getElementById('app-root'));
 
     // If you want your app to work offline and load faster, you can change
     // unregister() to register() below. Note this comes with some pitfalls.
