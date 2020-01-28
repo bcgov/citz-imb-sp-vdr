0e776ffb435f6ae9bef24501c45e05cca7f38b81
SDToews <33942967+SDToews@users.noreply.github.com>
2020-01-28 12:25:30 -0800

    Sprint 3 (#6)
    * daily commit
    
    * Update package-lock.json
    
    * daily commit
    
    * daily commit
    
    * Removing ignored files
    
    * morning commit
    
    * removed public from gitignore
    
    * daily commit
    
    * Final Sprint 3 Commit
    
    * add app-root div to home page
    
    * daily commit

--- src/components/tabs/VDRTabs.js
+++ src/components/tabs/VDRTabs.js
@@ -1,79 +1,101 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+import React from "react";
+import PropTypes from "prop-types";
+import { AppBar, Typography, Box, Tabs, Paper } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Tab from "@material-ui/core/Tab";
 
-import HomeIcon from '@material-ui/icons/Home';
-import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
-import SettingsIcon from '@material-ui/icons/Settings';
+import HomeIcon from "@material-ui/icons/Home";
+import QuestionAnswerIcon from "@material-ui/icons/QuestionAnswer";
+import SettingsIcon from "@material-ui/icons/Settings";
+import QuestionTabContent from "./questions/QuestionTabContent";
+
+import Proponents from "./sitemanagement/Proponents";
+import Home from "./home/Home";
 
 function TabPanel(props) {
-    const { children, value, index, ...other } = props;
+  const { children, value, index, ...other } = props;
 
-    return (
-        <Typography
-            component="div"
-            role="tabpanel"
-            hidden={value !== index}
-            id={`simple-tabpanel-${index}`}
-            aria-labelledby={`simple-tab-${index}`}
-            {...other}
-        >
-            {value === index && <Box p={3}>{children}</Box>}
-        </Typography>
-    );
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box p={3}>{children}</Box>}
+    </Typography>
+  );
 }
 
 TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.any.isRequired,
-    value: PropTypes.any.isRequired,
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired
 };
 
 function a11yProps(index) {
-    return {
-        id: `simple-tab-${index}`,
-        'aria-controls': `simple-tabpanel-${index}`,
-    };
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`
+  };
 }
 
 const useStyles = makeStyles(theme => ({
-    root: {
-        flexGrow: 1,
-        backgroundColor: theme.palette.background.paper,
-    },
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper
+  }
 }));
 
 export default function VDRTabs() {
-    const classes = useStyles();
-    const [value, setValue] = React.useState(0);
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
-    return (
-        <div className={classes.root}>
-            <AppBar position="static">
-                <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
-                    <Tab label="Home" icon={<HomeIcon />} {...a11yProps(0)} />
-                    <Tab label="Questions" icon={<QuestionAnswerIcon />} {...a11yProps(1)} />
-                    <Tab label="Site Management" icon={<SettingsIcon />} {...a11yProps(2)} />
-                </Tabs>
-            </AppBar>
-            <TabPanel value={value} index={0}>
-                Home
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="simple tabs example"
+        >
+          <Tab label="Home" icon={<HomeIcon />} {...a11yProps(0)} />
+          <Tab
+            label="Questions"
+            icon={<QuestionAnswerIcon />}
+            {...a11yProps(1)}
+          />
+          <Tab
+            label="Proponent Management"
+            icon={<SettingsIcon />}
+            {...a11yProps(2)}
+          />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        <Paper>
+          <h2>Home</h2>
+          <Home />
+        </Paper>
       </TabPanel>
-            <TabPanel value={value} index={1}>
-                Questions
+      <TabPanel value={value} index={1}>
+        <Paper>
+          <h2>Questions</h2>
+          <QuestionTabContent />
+        </Paper>
       </TabPanel>
-            <TabPanel value={value} index={2}>
-                Site Management
+      <TabPanel value={value} index={2}>
+        <Paper>
+          <h2>Proponent Management</h2>
+          <Proponents />
+        </Paper>
       </TabPanel>
-        </div>
-    );
+    </div>
+  );
 }
--- src/components/terms/TermsOfReference.js
+++ src/components/terms/TermsOfReference.js
@@ -1,21 +1,58 @@
 import React, { Component } from 'react';
 import { Dialog, DialogActions, DialogTitle, DialogContent, Button } from '@material-ui/core'
+import { setCookie } from '../utilities/cookies'
+import $ from 'jquery'
 
 export class TermsOfReference extends Component {
     constructor(props) {
         super(props)
-
+        console.log("props", props)
         this.state = {
+            title: 'VICO Terms of Reference',
+            body: 'Do not steal this stuff',
+            modified: 'insertmoment',
+            cookieDays: 1,
+            cookie: props.cookieName
         }
     }
 
     handleAgree = () => {
-        this.props.agreementCallback()
+        console.log("handleAgree")
+
+        this.setState({
+            cookie: this.props.cookieName + this.state.modified
+        })
+
+        setCookie(this.state.cookie, "true", this.state.cookieDays);
+        this.props.successCallback(this.state.cookie)
     }
 
     handleDisagree = () => {
         window.close()
         window.location = '/_layouts/signout.aspx'
+    }
+
+    componentDidMount = () => {
+        const _this = this
+
+        $.ajax({
+            url: "../_api/Web/Lists/getbytitle('Config')/items?$filter=Key eq 'TOR'&$select=TextValue,MultiTextValue,Modified,NumberValue",
+            type: "GET",
+            async: false,
+            headers: {
+                'Accept': 'application/json;odata=verbose'
+            }
+        }).done(function (data) {
+            _this.setState({
+                title: data.d.results[0].TextValue,
+                body: data.d.results[0].MultiTextValue,
+                modified: data.d.results[0].modified,
+                cookieDays: data.d.results[0].NumberValue
+            })
+            this.props.successCallBack({ modified: data.d.results[0].modified })
+        }).fail(function (err) {
+            window.console && console.warn("Error is Expected if page loaded outside sharepoint", err);
+        });
     }
 
     render() {
@@ -29,12 +66,12 @@ export class TermsOfReference extends Component {
                 aria-describedby="tor-dialog-description"
             >
                 <DialogTitle id="tor-dialog-title">
-                    {this.props.title}
+                    {this.state.title}
                 </DialogTitle>
                 <DialogContent
                     dividers={true}
                 >
-                    <div dangerouslySetInnerHTML={{ __html: this.props.body }} />
+                    <div dangerouslySetInnerHTML={{ __html: this.state.body }} />
                 </DialogContent>
 
                 <DialogActions>
