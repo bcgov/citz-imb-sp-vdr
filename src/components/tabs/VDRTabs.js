e9532272d3584beb7446b3243380390cfd6a9c19
SDToews <33942967+SDToews@users.noreply.github.com>
2020-06-09 04:05:30 -0700

    Ie compatible (#11)
    * secured site management
    
    * ie polyfill

--- src/components/tabs/VDRTabs.js
+++ src/components/tabs/VDRTabs.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { AppBar, Typography, Box, Tabs, Paper } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
@@ -8,9 +8,15 @@ import HomeIcon from '@material-ui/icons/Home'
 import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer'
 import SettingsIcon from '@material-ui/icons/Settings'
 
-// import { Private } from './private/Private'
+import { Private } from './private/Private'
 import { Public } from './public/Public'
 import { SiteManagement } from './sitemanagement/SiteManagement'
+
+import {
+	GetAssociatedGroups,
+	GetGroupMembers,
+	GetCurrentUser,
+} from 'citz-imb-sp-utilities'
 
 /**
  * Present the tabs for the site
@@ -35,39 +41,60 @@ function TabPanel(props) {
 TabPanel.propTypes = {
 	children: PropTypes.node,
 	index: PropTypes.any.isRequired,
-	value: PropTypes.any.isRequired
+	value: PropTypes.any.isRequired,
 }
 
 function a11yProps(index) {
 	return {
 		id: `simple-tab-${index}`,
-		'aria-controls': `simple-tabpanel-${index}`
+		'aria-controls': `simple-tabpanel-${index}`,
 	}
 }
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
 	root: {
 		flexGrow: 1,
 		backgroundColor: theme.palette.background.paper,
 		'& span.MuiTab-wrapper': {
-			textTransform: 'capitalize'
+			textTransform: 'capitalize',
 		},
 		'& button.MuiTab-textColorSecondary': {
-			color: '#efefef'
+			color: '#efefef',
 		},
 		'& button.MuiTab-textColorSecondary.Mui-selected': {
-			color: '#E3A82B'
-		}
-	}
+			color: '#E3A82B',
+		},
+	},
 }))
 
 export const VDRTabs = () => {
 	const classes = useStyles()
-	const [value, setValue] = React.useState(0)
+	const [value, setValue] = useState(0)
+	const [isManager, setIsManager] = useState(false)
 
 	const handleChange = (event, newValue) => {
 		setValue(newValue)
 	}
+
+	useEffect(() => {
+		Promise.all([
+			GetAssociatedGroups(),
+			GetCurrentUser({})
+		])
+		.then((response) => {
+			const [assocGroups, currentUser] = response
+			GetGroupMembers({ groupId: assocGroups.AssociatedOwnerGroup.Id }).then(
+				(groupMembers) => {
+					for(let i=0;i<groupMembers.length;i++){
+						if(currentUser.Id === groupMembers[i].Id){
+							setIsManager(true)
+						}
+					}
+				}
+			)
+		})
+		return () => {}
+	}, [])
 
 	return (
 		<div className={classes.root}>
@@ -87,11 +114,15 @@ export const VDRTabs = () => {
 						icon={<QuestionAnswerIcon />}
 						{...a11yProps(1)}
 					/>
-					<Tab
-						label='Site Management'
-						icon={<SettingsIcon />}
-						{...a11yProps(2)}
-					/>
+					{isManager ? (
+						<Tab
+							label='Site Management'
+							icon={<SettingsIcon />}
+							{...a11yProps(2)}
+						/>
+					) : (
+						''
+					)}
 				</Tabs>
 			</AppBar>
 			<TabPanel value={value} index={0}>
@@ -102,15 +133,18 @@ export const VDRTabs = () => {
 			</TabPanel>
 			<TabPanel value={value} index={1}>
 				<Paper>
-					<h2>Questions</h2>
-					{/* <Private /> */}
+					<Private />
 				</Paper>
 			</TabPanel>
-			<TabPanel value={value} index={2}>
-				<Paper>
-					<SiteManagement />
-				</Paper>
-			</TabPanel>
+			{isManager ? (
+				<TabPanel value={value} index={2}>
+					<Paper>
+						<SiteManagement />
+					</Paper>
+				</TabPanel>
+			) : (
+				''
+			)}
 		</div>
 	)
 }
--- src/components/tabs/private/Private.js
+++ src/components/tabs/private/Private.js
@@ -1,20 +1,21 @@
 import React, { useEffect, useState } from 'react'
-//import { SPList } from 'citz-imb-sp-utilities'
+import { GetCurrentUser, GetListItems } from 'citz-imb-sp-utilities'
 import { Container, Grid, Paper } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
+import { SPList } from '../../sharepoint/SPList'
 
 export const Private = () => {
-	const classes = makeStyles(theme => ({
+	const classes = makeStyles((theme) => ({
 		root: {
-			flexGrow: 1
+			flexGrow: 1,
 		},
 		paper: {
 			height: 140,
-			width: 100
+			width: 100,
 		},
 		control: {
-			padding: theme.spacing(2)
-		}
+			padding: theme.spacing(2),
+		},
 	}))
 
 	const options = {
@@ -23,49 +24,81 @@ export const Private = () => {
 		paging: false,
 		pageSize: 20,
 		draggable: false,
-		actionsColumnIndex: -1
+		actionsColumnIndex: -1,
 	}
 
-	const [privateTab, setPrivateTab] = useState('')
+	const [libraryName, setLibraryName] = useState()
+	const [listName, setListName] = useState()
+	const [isLoaded, setIsLoaded] = useState(false)
 
 	useEffect(() => {
+		Promise.all([
+			GetCurrentUser({ expand: 'Groups' }),
+			GetListItems({ listName: 'Proponents' }),
+		]).then((response) => {
+			const [currentUser, listItems] = response
+
+			for (let i = 0; i < listItems.length; i++) {
+				for (let j = 0; j < currentUser.Groups.results.length; j++) {
+					if (
+						currentUser.Groups.results[j].Id ===
+						listItems[i].GroupId
+					) {
+						setLibraryName(listItems[i].UUID)
+						setListName(`${listItems[i].UUID}_Questions`)
+						setIsLoaded(true)
+					}
+				}
+			}
+		})
 
 		return () => {}
 	}, [])
 
-	return (
-		<Container maxWidth='xl'>
-			<Paper variant='outlined'>{privateTab}</Paper>
-			<Grid container className={classes.root} spacing={2}>
-				<Grid item xs={12}>
-					<Grid container justify='center' spacing={2}>
-						<Grid key={'Documents'} item xs={6}>
-							<Paper className={classes.paper}>
-								<SPList
-									listName='Documents'
-									// addItem={false}
-									// deleteItem={false}
-									// editItem={false}
-									// changeItemPermission={false}
-									options={options}
-								/>
-							</Paper>
-						</Grid>
-						<Grid key={'Questions'} item xs={6}>
-							<Paper className={classes.paper}>
-								<SPList
-									listName='Questions'
-									// addItem={false}
-									// deleteItem={false}
-									// editItem={false}
-									// changeItemPermission={false}
-									options={options}
-								/>
-							</Paper>
+	if (isLoaded) {
+		return (
+			<Container maxWidth='xl'>
+				<Grid container className={classes.root} spacing={2}>
+					<Grid item xs={12}>
+						<Grid container justify='center' spacing={2}>
+							<Grid key={libraryName} item xs={6}>
+								<Paper className={classes.paper}>
+									<SPList
+										listName={libraryName}
+										addItem={false}
+										// deleteItem={false}
+										// editItem={false}
+										// changeItemPermission={false}
+										options={options}
+									/>
+								</Paper>
+							</Grid>
+							<Grid key={listName} item xs={6}>
+								<Paper className={classes.paper}>
+									<SPList
+										listName={listName}
+										// addItem={false}
+										// deleteItem={false}
+										// editItem={false}
+										// changeItemPermission={false}
+										options={options}
+									/>
+								</Paper>
+							</Grid>
 						</Grid>
 					</Grid>
 				</Grid>
-			</Grid>
-		</Container>
-	)
+			</Container>
+		)
+	} else {
+		return (
+			<Container maxWidth='xl'>
+				<Grid container className={classes.root} spacing={2}>
+					<Grid item xs={12}>
+						<div>No associated Proponent</div>
+					</Grid>
+				</Grid>
+			</Container>
+		)
+	}
 }
