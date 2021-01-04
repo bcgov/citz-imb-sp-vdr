092439e0b89a797314130d834cac50b9a20cfc02
SDToews <33942967+SDToews@users.noreply.github.com>
2021-01-04 10:30:27 -0800

    fixes tab content over-run

--- src/components/PublicTab/Public/Public.js
+++ src/components/PublicTab/Public/Public.js
@@ -16,7 +16,7 @@ const useStyles = makeStyles((theme) => ({
 		flexGrow: 1,
 		backgroundColor: theme.palette.background.paper,
 		display: 'flex',
-		height: 224,
+		// height: 224,
 	},
 	tabs: {
 		borderRight: `1px solid ${theme.palette.divider}`,
