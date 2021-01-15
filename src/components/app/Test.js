5894b87b3d5e96a89fa6edf57a1fb6b5836d7790
SDToews <33942967+SDToews@users.noreply.github.com>
2021-01-15 08:02:28 -0800

    VICO-103

--- src/components/app/Test.js
+++ src/components/app/Test.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Questions } from 'components/ProponentManagementTab/Questions/Questions'
 import {FormikTest} from 'components/Reusable/Formik/FormikTest'
-import {ProponentManagementTab} from 'components/ProponentManagementTab/ProponentManagementTab'
+import {Proponent} from 'components/ProponentTab/Proponent/Proponent'
 
 export const Test = () => {
 	const options = {UUID:'V68C58C'}
 
-	return <ProponentManagementTab />
+	return <Proponent {...options} />
 	// return <FormikTest open={true} close={()=>{}} />
 }
