450f03194e65afc0b7cff0e9da9ba4853892fb42
SDToews <33942967+SDToews@users.noreply.github.com>
2021-04-13 10:39:30 -0700

    proponents can now delete documents

--- src/components/proponenttab/ProponentLibrary/ProponentLibrary.js
+++ src/components/proponenttab/ProponentLibrary/ProponentLibrary.js
@@ -1,21 +1,22 @@
-import { Alert } from '@material-ui/lab'
-import { useCurrentUser } from 'components'
-import { SPLibrary } from 'components/SharePoint'
-import React from 'react'
+import { Alert } from '@material-ui/lab';
+import { useCurrentUser } from 'components';
+import { SPLibrary } from 'components/SharePoint';
+import React from 'react';
 
 export const ProponentLibrary = () => {
-	const currentUser = useCurrentUser()
+	const currentUser = useCurrentUser();
 
-	const listName = currentUser.proponent
+	const listName = currentUser.proponent;
 
 	if (!currentUser.isProponent)
-		return <Alert severity={'info'}>User is not a proponent</Alert>
+		return <Alert severity={'info'}>User is not a proponent</Alert>;
 
 	return (
 		<SPLibrary
 			listName={listName}
 			title={'Submitted Documents'}
 			allowUpload={true}
+			allowDelete={true}
 		/>
-	)
-}
+	);
+};
