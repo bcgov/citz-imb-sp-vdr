f7a78096029d46739679b7ef7c90a239ee494b44
SDToews <33942967+SDToews@users.noreply.github.com>
2021-04-13 11:06:44 -0700

    updated acivity list to use splist (#47)

--- src/components/sitemanagementtab/ActivityLog/ActivityLog.js
+++ src/components/sitemanagementtab/ActivityLog/ActivityLog.js
@@ -1,30 +1,9 @@
-import { LinearProgress } from '@material-ui/core';
-import { useList } from 'components';
-import React from 'react';
+import { SPList } from 'components/SharePoint';
+import React, { useMemo } from 'react';
 
 export const ActivityLog = () => {
-	const { isLoading, getRender, SelectColumnFilter } = useList('ActivityLog');
-
-	const listOptions = {
-		customColumns: [
-			{
-				Filter: SelectColumnFilter,
-				Footer: 'Proponent',
-				Header: 'Proponent',
-				accessor: 'Proponent',
-				disableFilters: false,
-				disableSortBy: true,
-			},
-			{
-				Filter: SelectColumnFilter,
-				Footer: 'User',
-				Header: 'User',
-				accessor: 'User',
-				disableFilters: false,
-				disableSortBy: true,
-			},
-		],
-	};
-
-	return isLoading ? <LinearProgress /> : getRender(listOptions);
+	const initialState = useMemo(() => {
+		return { sortBy: [{ id: 'Modified', desc: true }], pageSize: 50 };
+	}, []);
+	return <SPList listName={'ActivityLog'} initialState={initialState} />;
 };
