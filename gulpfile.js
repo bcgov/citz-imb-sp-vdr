90cced65706bc822d0b9bc05ed04f5d9e9174d07
SDToews <33942967+SDToews@users.noreply.github.com>
2021-04-19 07:56:24 -0700

    daily commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,25 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 
 gulp.task('build-script', function () {
-    return gulp.src('./build/static/js/*.js')
-        .pipe(concat('vdr.js'))
-        .pipe(gulp.dest('./dist/VirtualDocumentRoomModules/Files/SiteAssets/js'))
-        .pipe(gulp.dest('z:/VDRoom/SiteAssets/js'))  //dev
-        .pipe(gulp.dest('y:/VDRoom/SiteAssets/js'))  //uat
+	return gulp
+		.src('./build/static/js/*.js')
+		.pipe(concat('vdr.js'))
+		.pipe(
+			gulp.dest('./dist/VirtualDocumentRoomModules/Files/SiteAssets/js')
+		)
+		.pipe(gulp.dest('a:/VDRoom/SiteAssets/js')) //dev
+		.pipe(gulp.dest('b:/VDRoom/SiteAssets/js')); //uat
 });
 
 gulp.task('build-css', function () {
-    return gulp.src('./build/static/css/*.css')
-        .pipe(concat('vdr.css'))
-        .pipe(gulp.dest('./dist/VirtualDocumentRoomModules/Files/SiteAssets/css'))
+	return gulp
+		.src('./build/static/css/*.css')
+		.pipe(concat('vdr.css'))
+		.pipe(
+			gulp.dest('./dist/VirtualDocumentRoomModules/Files/SiteAssets/css')
+		)
+		.pipe(gulp.dest('a:/VDRoom/SiteAssets/js')) //dev
+		.pipe(gulp.dest('b:/VDRoom/SiteAssets/js')); //uat
 });
 
-gulp.task('default', gulp.parallel(['build-script', 'build-css']))
+gulp.task('default', gulp.parallel(['build-script', 'build-css']));
