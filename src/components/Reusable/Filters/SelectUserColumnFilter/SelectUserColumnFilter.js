b671fe3cb3d91b4ee793d698d6ee9f6881a6d81f
SDToews <33942967+SDToews@users.noreply.github.com>
2021-04-26 14:34:46 -0700

    fixed editor filter

--- src/components/Reusable/Filters/SelectUserColumnFilter/SelectUserColumnFilter.js
+++ src/components/Reusable/Filters/SelectUserColumnFilter/SelectUserColumnFilter.js
@@ -3,43 +3,43 @@ import React, { useCallback, useEffect, useState } from 'react';
 
 export const SelectUserColumnFilter = (props) => {
 	const {
-		column: { filterValue, setFilter, preFilteredRows },
-	} = props;
+		column: { filterValue, setFilter, preFilteredRows, id },
+	} = props
 
-	const [options, setOptions] = useState();
+	const [options, setOptions] = useState()
 
 	const getOptions = useCallback(async () => {
 		const uniqueUserIds = preFilteredRows
-			.map((row) => row.values.AuthorId)
-			.filter((id, index, self) => self.indexOf(id) === index);
+			.map((row) => row.values[id])
+			.filter((id, index, self) => self.indexOf(id) === index)
 
-		let userOptions = [];
+		let userOptions = []
 
 		for (let i = 0; i < uniqueUserIds.length; i++) {
-			const user = await GetUser({ userId: uniqueUserIds[i] });
+			const user = await GetUser({ userId: uniqueUserIds[i] })
 			userOptions.push(
 				<option key={i} value={uniqueUserIds[i]}>
 					{user.Title}
 				</option>
-			);
+			)
 		}
 
-		setOptions(userOptions);
-	}, [preFilteredRows]);
+		setOptions(userOptions)
+	}, [preFilteredRows])
 
 	useEffect(() => {
-		getOptions();
-		return () => {};
-	}, [getOptions]);
+		getOptions()
+		return () => {}
+	}, [getOptions])
 
 	return (
 		<select
 			value={filterValue}
 			onChange={(e) => {
-				setFilter(e.target.value || undefined);
+				setFilter(e.target.value || undefined)
 			}}>
 			<option value=''>All</option>
 			{options}
 		</select>
-	);
-};
+	)
+}
