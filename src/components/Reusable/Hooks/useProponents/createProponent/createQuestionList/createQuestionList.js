fb0d8a2f45e9e4a82d4784f5f63608c9580b6eaf
SDToews <33942967+SDToews@users.noreply.github.com>
2021-04-28 08:35:35 -0700

    Permissions applied to proponent questions list

--- src/components/Reusable/Hooks/useProponents/createProponent/createQuestionList/createQuestionList.js
+++ src/components/Reusable/Hooks/useProponents/createProponent/createQuestionList/createQuestionList.js
@@ -1,16 +1,21 @@
 import {
 	AddFieldToList,
 	AddListViewField,
+	AddPermissionsToList,
 	BreakListPermissionsInheritance,
 	CreateList,
 	CreateView,
 	GetListDefaultView,
 	RemoveListViewAllFields,
+	RemovePermissionsFromList,
 	UpdateField,
-} from 'components/ApiCalls';
+} from 'components/ApiCalls'
 
-export const createQuestionList = async (listName) => {
-	await CreateList({ listName });
+export const createQuestionList = async (
+	listName,
+	{ currentUser, roles, associatedGroups }
+) => {
+	await CreateList({ listName })
 
 	await AddFieldToList({
 		listName,
@@ -44,59 +49,86 @@ export const createQuestionList = async (listName) => {
 				DefaultValue: '0',
 			},
 		],
-	});
+	})
 	await UpdateField({
 		listName,
 		fieldName: 'AnswerStatus',
 		field: { Title: 'Answer Status' },
-	});
+	})
 
 	await UpdateField({
 		listName,
 		fieldName: 'Title',
 		field: { Required: false },
-	});
+	})
 
 	await UpdateField({
 		listName,
 		fieldName: 'Created By',
 		field: { Title: 'Submitted By' },
-	});
-	const defaultView = await GetListDefaultView({ listName });
-	const viewGUID = defaultView.Id;
+	})
+	const defaultView = await GetListDefaultView({ listName })
+	const viewGUID = defaultView.Id
 
-	await RemoveListViewAllFields({ listName, viewGUID });
+	await RemoveListViewAllFields({ listName, viewGUID })
 
-	await AddListViewField({ listName, viewGUID, field: 'Question' });
+	await AddListViewField({ listName, viewGUID, field: 'Question' })
 
-	await AddListViewField({ listName, viewGUID, field: 'AnswerStatus' });
+	await AddListViewField({ listName, viewGUID, field: 'AnswerStatus' })
 
-	await AddListViewField({ listName, viewGUID, field: 'Submitted By' });
+	await AddListViewField({ listName, viewGUID, field: 'Submitted By' })
 
-	await AddListViewField({ listName, viewGUID, field: 'Created' });
+	await AddListViewField({ listName, viewGUID, field: 'Created' })
 
 	const VICOManagerView = await CreateView({
 		listName,
 		viewName: 'VICO_Manager',
-	});
+	})
 
-	const managerViewId = VICOManagerView.Id;
+	const managerViewId = VICOManagerView.Id
 
 	await AddListViewField({
 		listName,
 		viewGUID: managerViewId,
 		field: 'QuestionID',
-	});
+	})
 
 	await AddListViewField({
 		listName,
 		viewGUID: managerViewId,
 		field: 'Assignee',
-	});
+	})
 
 	await BreakListPermissionsInheritance({
 		listName,
 		copy: false,
 		clear: true,
-	});
-};
+	})
+
+	await AddPermissionsToList({
+		//owners
+		listName,
+		principalId: associatedGroups.AssociatedOwnerGroup.Id,
+		roleDefId: roles['Full Control'].Id,
+	})
+
+	await AddPermissionsToList({
+		//members
+		listName,
+		principalId: associatedGroups.AssociatedMemberGroup.Id,
+		roleDefId: roles['Contribute'].Id,
+	})
+
+	await AddPermissionsToList({
+		//visitors
+		listName,
+		principalId: associatedGroups.AssociatedVisitorGroup.Id,
+		roleDefId: roles['Read'].Id,
+	})
+
+	await RemovePermissionsFromList({
+		listName,
+		principalId: currentUser.id,
+		roleDefId: roles['Full Control'].Id,
+	})
+}
