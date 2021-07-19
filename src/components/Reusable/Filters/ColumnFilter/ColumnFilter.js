ec7ef9d8112911342a89c9db3851eff69ee042a5
SDToews <33942967+SDToews@users.noreply.github.com>
2021-07-19 13:29:02 -0700

    filter placeholder changed

--- src/components/Reusable/Filters/ColumnFilter/ColumnFilter.js
+++ src/components/Reusable/Filters/ColumnFilter/ColumnFilter.js
@@ -2,12 +2,13 @@ import { Input } from '@material-ui/core'
 import React from 'react'
 
 export const ColumnFilter = ({ column }) => {
-	const { filterValue, setFilter } = column
+	const { filterValue, setFilter, Header } = column
 	return (
 		<Input
-			placeholder={'filter column'}
+			placeholder={`type here to filter by ${Header}`}
 			color={'primary'}
 			value={filterValue || ''}
+			fullWidth={true}
 			onChange={(e) => {
 				e.preventDefault()
 				return setFilter(e.target.value)
